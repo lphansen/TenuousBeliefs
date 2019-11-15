ff2226a25064c10885c33ba0896f27adbb4c119f
andyjmwang <jiamingwang@uchicago.edu>
2019-11-15 14:33:51 -0600

    Remove unnecessary files

--- MATLAB/Calibrate_theta_with_USrelativeEntropy/CalibrateTheta.m
+++
@@ -1,9 +0,0 @@
-function diff = CalibrateTheta(theta,param,dv0,zl,zr,dvl,dvr,Dz,tgtUSq)
-% Modified from the function HL
-
-[pol,~,~] = MatchODE(theta,zl,zr,dvl,dvr,param,Dz,dv0);
-[tort,~,~] = Distortion(pol,theta,param);
-qUS = RelativeEntropyUS(tort(3:4,:),tort(1:2,:),pol.x,param); 
-diff = qUS-tgtUSq;
-
-end
--- MATLAB/KolmogorovForward/InterpQuantile.m
+++
@@ -1,9 +0,0 @@
-function qt = InterpQuantile(zgrid,mgrid,z0)
-
-dim = size(mgrid);
-qt = [];
-for t = 1:dim(2)
-    tmp = interp1(zgrid,mgrid(:,t),z0);
-    qt = [qt tmp];
-end    
-    
